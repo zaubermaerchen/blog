cffa6a432fe8cde942e733832df64d91fd201289
Mutsuki Kimuraya <zaubermaerchen@me.com>
2024-01-06 17:27:09 +0900

    netlify上で動かす為の設定追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog.zaubermaerchen.info',
-	integrations: [mdx(), sitemap()],
+  site: 'https://blog.zaubermaerchen.info',
+  integrations: [mdx(), sitemap()],
+  output: "server",
+  adapter: netlify()
 });
