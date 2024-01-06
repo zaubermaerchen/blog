99c7fe5e0a7b21c4b8fc09aa22ffc29c8de7fd2e
Mutsuki Kimuraya <zaubermaerchen@me.com>
2024-01-06 18:13:41 +0900

    静的サイトなので@astrojs/netlifyはいらなかった…

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import netlify from "@astrojs/netlify";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.zaubermaerchen.info',
-  integrations: [mdx(), sitemap()],
-  adapter: netlify()
+  integrations: [mdx(), sitemap()]
 });
