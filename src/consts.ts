80832febbb27dbba056aac0540056043642e1e2a
Mutsuki Kimuraya <zaubermaerchen@me.com>
2024-01-06 17:11:25 +0900

    ページレイアウトカスタマイズ

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = '雑記帖';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const DEFAULT_HERO_IMAGE_PATH = '/no-image.jpg';
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,16 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const posts = (await getCollection('blog')).sort(
+        (a, b) => Math.sign(b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
+    );
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/blog/${post.slug}/`,
+			link: `/${post.slug}/`,
 		})),
 	});
 }
