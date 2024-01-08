625bc209420926a5f28a7b21f5b60076f2384889
Mutsuki Kimuraya <zaubermaerchen@me.com>
2024-01-08 15:09:33 +0900

    レイアウト調整

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = '雑記帖';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'メモ置き場';
 export const DEFAULT_HERO_IMAGE_PATH = '/no-image.jpg';
